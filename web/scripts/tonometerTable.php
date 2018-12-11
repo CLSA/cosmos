cc9f4ffab63fbe987b6601f91047695e84febb72
Dean Inglis <inglisd@mcmaster.ca>
2018-12-11 13:51:57 -0500

    BUG: add missing post vars for par limits

--- web/scripts/tonometerTable.php
+++ web/scripts/tonometerTable.php
@@ -4,6 +4,8 @@ require_once 'tonometer_generator.class.php';
 $begin_date = htmlspecialchars($_POST['from']);
 $end_date = htmlspecialchars($_POST['to']);
 $rank = htmlspecialchars($_POST['rank']);
+$par_min = htmlspecialchars($_POST['par-qac-min']);
+$par_max = htmlspecialchars($_POST['par-qac-max']);
 
 $tonometer = new tonometer_generator('tonometer', $rank, $begin_date, $end_date);
 $tonometer->build_table_data();
