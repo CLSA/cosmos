3c5c5e185fa4a50695f1c6d02a505120255022e7
Patrick Emond <emondpd@mcmaster.ca>
2019-05-07 09:47:28 -0400

    Updating stage module list

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'cosmos';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.4';
-$SETTINGS['general']['build'] = '100790c';
+$SETTINGS['general']['build'] = '55d17be';
 
 // the location of the application's internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
