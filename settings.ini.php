dbdb2e9dd6e1fe7ca3e7e99329595f1b33742fec
Patrick Emond <emondpd@mcmaster.ca>
2019-05-15 14:15:20 -0400

    Fixing bugs

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'cosmos';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.4';
-$SETTINGS['general']['build'] = '846130a';
+$SETTINGS['general']['build'] = '30dae53';
 
 // the location of the application's internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
