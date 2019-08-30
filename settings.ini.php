b3a375149eb4ff3a49ff1c4e7af64308dbdaba89
Patrick Emond <emondpd@mcmaster.ca>
2019-08-30 16:21:20 -0400

    Marking the start of version 2.5

--- settings.ini.php
+++ settings.ini.php
@@ -12,7 +12,7 @@ global $SETTINGS;
 // tagged version
 $SETTINGS['general']['application_name'] = 'cosmos';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
-$SETTINGS['general']['version'] = '2.4';
+$SETTINGS['general']['version'] = '2.5';
 $SETTINGS['general']['build'] = '07369f1';
 
 // the location of the application's internal path
